d79aa1f98f13a5014ee430fa0185e50574774181
secretbase1213 <shuhei.hasegawa1989@gmail.com>
2021-12-03 08:17:21 +0900

    CORSエラー対応v4

--- components/List.vue
+++ components/List.vue
@@ -54,8 +54,9 @@
 
 <script>
 import axios from 'axios';
-axios.defaults.headers.post['Content-Type'] = 'application/json;charset=utf-8';
-axios.defaults.headers.post['Access-Control-Allow-Origin'] = '*';
+axios.defaults.baseURL = 'https://linq-best-ten.netlify.app/';
+axios.defaults.headers.get['Content-Type'] = 'application/json;charset=utf-8';
+axios.defaults.headers.get['Access-Control-Allow-Origin'] = '*';
 
 export default {
   name: 'List',
