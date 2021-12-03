dafa112cc57a15e0d4423dbde7da877e65ee9a8b
secretbase1213 <shuhei.hasegawa1989@gmail.com>
2021-12-03 22:13:09 +0900

    title属性を修正

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'linq-best-ten',
+    title: 'あなたが選ぶLinQ楽曲ベスト10',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
